074a5f5cb8e651fd6abc4e949c66126cce1a6284
Andre <cavallari@live.com>
2014-04-25 15:48:31 -0300

    Starting up project

--- Controller/ThumbsAppController.php
+++ Controller/ThumbsAppController.php
@@ -16,19 +16,31 @@ class ThumbsAppController extends AppController{
 
     protected $size;
     protected $path;
+    protected $hash;
+    protected $cache_path;
 
     public function __construct($request = null, $response = null) {
         parent::__construct($request, $response);
+
+        //Lê o arquivo de config, caso não existir irá usar o padrão
         $config = realpath(APP."/Config/thumbs.php");
         if($config) $this->sizes = include($config);
     }
 
     public function size(){
+        //Pega todos os argumentos enviados
         $arguments = func_get_args();
-        $this->size = array_shift($arguments);
-        $this->size = $this->sizes[$this->request->params['controller']][$this->size];
+
+        //Pega o primeiro item dos argumentos enviados e retira do array, no caso o tamanho (tiny,small,medium,etc)
+        $size = array_shift($arguments);
+        $this->size = $this->sizes[$this->request->params['controller']][$size];
+
+        //Configura o caminho absoluto da imagem
         $this->path = implode("/",$arguments);
         $this->path = realpath(APP.WEBROOT_DIR.DS.$this->path);
+
+        if(!$this->path) throw new NotFoundException(__('Imagem não encontrada'));
+
         pr($this->path);
         pr($this->size);
     }
