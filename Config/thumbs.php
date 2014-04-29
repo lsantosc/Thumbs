e2e5c5504f4607a38ea1f420f5a83a4b2879665f
Andre <cavallari@live.com>
2014-04-29 13:47:50 -0300

    Fix PNG generation, now with alpha

---
+++ Config/thumbs.php
@@ -0,0 +1,21 @@
+<?PHP
+return array(
+    'crop'=>array(
+        'tiny'=>array(50,50),
+        'small'=>array(100,100),
+        'medium'=>array(200,200),
+    ),
+    'resize'=>array(
+        'tiny'=>array(100,100),
+        'small'=>array(300,300),
+        'medium'=>array(600,600),
+    ),
+    'fill'=>array(
+        'tiny'=>array(50,50),
+        'small'=>array(100,100),
+        'medium'=>array(200,200),
+    ),
+    'colors'=>array(
+
+    ),
+);
