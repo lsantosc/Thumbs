bbf6438dc0c88c82514b7bf24748e49372641da9
Andre <cavallari@live.com>
2014-04-29 10:18:35 -0300

    Atualizando projeto

---
+++ View/Helper/ThumbsHelper.php
@@ -0,0 +1,25 @@
+<?PHP
+class ThumbsHelper extends AppHelper{
+
+    public $helpers = array('Html');
+    public $path;
+
+    public function get($path){
+        $this->path = $path;
+        return $this;
+    }
+
+    public function crop($size){
+        return $this->Html->image("/thumbs/generate/crop/{$size}/{$this->path}");
+    }
+
+    public function resize($size){
+        return $this->Html->image("/thumbs/generate/resize/{$size}/{$this->path}");
+    }
+
+    public function fill($size,$fill){
+        return $this->Html->image("/thumbs/generate/fill/{$fill}/{$size}/{$this->path}");
+    }
+
+
+}
