a90406b2ebb92abfe9617cbaf0b69a020d893c21
Andre <cavallari@live.com>
2014-05-02 10:35:53 -0300

    Adding header 304 + Etag to cached images

--- Controller/ThumbsController.php
+++ Controller/ThumbsController.php
@@ -52,6 +52,9 @@ class ThumbsController extends ThumbsAppController{
 
     public function cached(){
         $config = $this->request->params['image'];
+        $filemtime = filemtime($config['path']);
+        header("Etag: $filemtime");
+        header("HTTP/1.1 304 Not Modified");
         header("Content-Type: {$config['mime']}");
         echo file_get_contents($config['thumb']);
         exit;
