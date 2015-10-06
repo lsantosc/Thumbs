25aed1422aa97c79ae6a919e063d1f04529104e4
Lucas Santos da Costa <lucas@amexcom.com.br>
2015-10-06 18:36:02 -0300

    Fix crop method

--- Vendor/Gd.php
+++ Vendor/Gd.php
@@ -5,6 +5,9 @@ class GdHandler{
     public $width;
     public $height;
     public $mime;
+    public $crop_position = array();
+    public $extension;
+    public $path;
 
     public function load($input){
         $this->mime = image_type_to_mime_type(exif_imagetype($input));
@@ -13,19 +16,17 @@ class GdHandler{
         imagealphablending($this->image,true);
         $this->width = imagesx($this->image);
         $this->height = imagesy($this->image);
+        $this->extension = @strtolower(pathinfo($input, PATHINFO_EXTENSION));
+        $this->path = $input;
     }
 
-    public function crop($width,$height){
-        $dw = $width;
-        $dh = $height;
-        $this->resize($width,$height,'min');
-        $sx = ($this->width/2) - ($width/2);
-        $sy = ($this->height/2) - ($height/2);
+    public function crop($width,$height) {
+        $this->calculateCropPositions($width,$height);
         $new = $this->create($width,$height);
-        imagecopyresampled($new,$this->image,0,0,$sx,$sy,$width,$height,$width,$height);
-        $this->image=$new;
-        $this->width=$width;
-        $this->height=$height;
+        imagecopyresampled($new, $this->image, -$this->crop_position[0], -$this->crop_position[1], 0, 0, $this->crop_position[2], $this->crop_position[3], $this->width, $this->height);
+        $this->image = $new;
+        $this->width = $width;
+        $this->height = $height;
     }
 
     public function resize($size,$method='width'){
@@ -43,7 +44,7 @@ class GdHandler{
         $diff = max($this->width/$width,$this->height/$height);
         $dw = floor($this->width/$diff);
         $dh = floor($this->height/$diff);
-         $temp = $this->create($dw,$dh);
+        $temp = $this->create($dw,$dh);
         imagecopyresampled($temp,$this->image,0,0,0,0,$dw,$dh,$this->width,$this->height);
         $this->width = $dw;
         $this->height = $dh;
@@ -59,6 +60,38 @@ class GdHandler{
         $this->width = $width;
         $this->height = $height;
     }
+
+
+    /*public function fill($width, $height, $fillColor)
+    {
+        $temp = $this->createFromType();
+        imagecopyresampled($temp,$this->image,0,0,0,0,$dw,$dh,$this->width,$this->height);
+        $color = $this->hexToRGB($fillColor);
+        $alpha = 127-intval(($color['alpha']/100)*127);
+
+        $new = $this->create($width, $height);
+        $color = imagecolorallocatealpha($new, $color['red'], $color['green'], $color['blue'], $alpha);
+        imagefill($new,0,0,$color);
+
+        if ( ($this->width / $width ) > ( $this->height / $height ) )
+        {
+            $fator = $this->width / $width;
+        } else {
+            $fator = $this->height / $height;
+        }
+
+        $dw = $this->width / $fator;
+        $dh = $this->height / $fator;
+
+        // copia com o novo tamanho, centralizando
+        $dx = ( $width - $dw ) / 2;
+        $dy = ( $height - $dh ) / 2;
+        imagecopyresampled($new, $temp, $dx, $dy, 0, 0, $dw, $dh, $width, $height);
+
+        $this->image = $new;
+        $this->width = $width;
+        $this->height = $height;
+    }*/
 
     public function hexToRGB($color){
         $r = hexdec(substr($color,0,2));
@@ -77,6 +110,27 @@ class GdHandler{
         imagesavealpha($image,true);
         imagefill($image,0,0,$alpha);
         return $image;
+    }
+
+    public function createFromType() {
+        switch($this->extension)
+        {
+            case 'gif':
+                return imagecreatefromgif($this->path);
+                break;
+            case 'jpg':
+                return imagecreatefromjpeg($this->path);
+                break;
+            case 'png':
+                return imagecreatefrompng($this->path);
+                break;
+            case 'bmp':
+                return $this->imagecreatefrombmp($this->path);
+                break;
+            default:
+                trigger_error('Invalid File', E_USER_ERROR);
+                break;
+        }
     }
 
     public function show($path = false){
@@ -103,4 +157,123 @@ class GdHandler{
             default: imagepng($this->image,$destination,$quality); break;
         }
     }
+
+    private function calculateCropPositions($width,$height)
+    {
+        // 50% of real sizes
+        $hm = $this->height / $height;
+        $wm = $this->width / $width;
+
+        // 50% of new sizes
+        $h_height = $height / 2;
+        $h_width  = $width / 2;
+
+        // calculate
+
+        if ($wm > $hm) {
+            $this->crop_position[2] = $this->width / $hm;
+            $this->crop_position[3] = $height;
+            $this->crop_position[0] = ($this->crop_position[2] / 2) - $h_width;
+            $this->crop_position[1] = 0;
+        }
+        // if width is less or equal of height
+        elseif (($wm <= $hm)) {
+            $this->crop_position[2] = $width;
+            $this->crop_position[3] = $this->height / $wm;
+            $this->crop_position[0] = 0;
+            $this->crop_position[1] = ($this->crop_position[3] / 2) - $h_height;
+        }
+
+    }
+
+    public function imagecreatefrombmp($filename) {
+        //Ouverture du fichier en mode binaire
+        if (! $f1 = fopen($filename,"rb")) return FALSE;
+
+        //1 : Chargement des ent?tes FICHIER
+        $FILE = unpack("vfile_type/Vfile_size/Vreserved/Vbitmap_offset", fread($f1,14));
+        if ($FILE['file_type'] != 19778) return FALSE;
+
+        //2 : Chargement des ent?tes BMP
+        $BMP = unpack('Vheader_size/Vwidth/Vheight/vplanes/vbits_per_pixel'.
+            '/Vcompression/Vsize_bitmap/Vhoriz_resolution'.
+            '/Vvert_resolution/Vcolors_used/Vcolors_important', fread($f1,40));
+        $BMP['colors'] = pow(2,$BMP['bits_per_pixel']);
+        if ($BMP['size_bitmap'] == 0) $BMP['size_bitmap'] = $FILE['file_size'] - $FILE['bitmap_offset'];
+        $BMP['bytes_per_pixel'] = $BMP['bits_per_pixel']/8;
+        $BMP['bytes_per_pixel2'] = ceil($BMP['bytes_per_pixel']);
+        $BMP['decal'] = ($BMP['width']*$BMP['bytes_per_pixel']/4);
+        $BMP['decal'] -= floor($BMP['width']*$BMP['bytes_per_pixel']/4);
+        $BMP['decal'] = 4-(4*$BMP['decal']);
+        if ($BMP['decal'] == 4) $BMP['decal'] = 0;
+
+        //3 : Chargement des couleurs de la palette
+        $PALETTE = array();
+        if ($BMP['colors'] < 16777216)
+        {
+            $PALETTE = unpack('V'.$BMP['colors'], fread($f1,$BMP['colors']*4));
+        }
+
+        //4 : Cr?ation de l'image
+        $IMG = fread($f1,$BMP['size_bitmap']);
+        $VIDE = chr(0);
+
+        $res = imagecreatetruecolor($BMP['width'],$BMP['height']);
+        $P = 0;
+        $Y = $BMP['height']-1;
+        while ($Y >= 0)
+        {
+            $X=0;
+            while ($X < $BMP['width'])
+            {
+                if ($BMP['bits_per_pixel'] == 24)
+                    $COLOR = @unpack("V",substr($IMG,$P,3).$VIDE);
+                elseif ($BMP['bits_per_pixel'] == 16)
+                {
+                    $COLOR = @unpack("n",substr($IMG,$P,2));
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 8)
+                {
+                    $COLOR = @unpack("n",$VIDE.substr($IMG,$P,1));
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 4)
+                {
+                    $COLOR = @unpack("n",$VIDE.substr($IMG,floor($P),1));
+                    if (($P*2)%2 == 0) $COLOR[1] = ($COLOR[1] >> 4) ; else $COLOR[1] = ($COLOR[1] & 0x0F);
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 1)
+                {
+                    $COLOR = @unpack("n",$VIDE.substr($IMG,floor($P),1));
+                    if     (($P*8)%8 == 0) $COLOR[1] =  $COLOR[1]          >>7;
+                    elseif (($P*8)%8 == 1) $COLOR[1] = ($COLOR[1] & 0x40)>>6;
+                    elseif (($P*8)%8 == 2) $COLOR[1] = ($COLOR[1] & 0x20)>>5;
+                    elseif (($P*8)%8 == 3) $COLOR[1] = ($COLOR[1] & 0x10)>>4;
+                    elseif (($P*8)%8 == 4) $COLOR[1] = ($COLOR[1] & 0x8)>>3;
+                    elseif (($P*8)%8 == 5) $COLOR[1] = ($COLOR[1] & 0x4)>>2;
+                    elseif (($P*8)%8 == 6) $COLOR[1] = ($COLOR[1] & 0x2)>>1;
+                    elseif (($P*8)%8 == 7) $COLOR[1] = ($COLOR[1] & 0x1);
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                else
+                    return FALSE;
+                imagesetpixel($res,$X,$Y,$COLOR[1]);
+                $X++;
+                $P += $BMP['bytes_per_pixel'];
+            }
+            $Y--;
+            $P+=$BMP['decal'];
+        }
+
+        //Fermeture du fichier
+        fclose($f1);
+
+        return $res;
+
+    } // fim function image from BMP
+
+
+
 }
